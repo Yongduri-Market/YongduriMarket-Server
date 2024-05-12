a50c2e9db1685b66334bd7f8d25d1f86b352fa79
dkswndus <dkswndus6988@naver.com>
2024-05-12 18:32:00 +0900

    feat: 프로젝트 생성

--- src/main/java/Graduation/work/YongduriMarketServer/dto/RegisterDto.java
+++ src/main/java/Graduation/work/YongduriMarketServer/dto/RegisterDto.java
@@ -14,20 +14,9 @@ public class RegisterDto {
     private String  pwd;
     private String  name;
     private String  nickname;
-    private Integer role;
-    private Integer genderCategory;
-    private String  img;
-    private Integer age;
-    private String method;
-    private Integer  fee;
-    private String  location1;
-    private String  location2;
-    private String  location3;
-    private String subject1;
-    private String subject2;
-    private String subject3;
-    private String subject4;
-    private String subject5;
+
+
+
 
 
 
