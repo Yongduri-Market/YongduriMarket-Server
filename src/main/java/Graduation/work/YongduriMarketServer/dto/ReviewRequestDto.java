a150f3d9c6fd60238d006be4faba8cdafc6b7db7
co9fla30 <kcl859@naver.com>
2024-07-02 23:02:14 +0900

    review 등록, 조회 추가

---
+++ src/main/java/Graduation/work/YongduriMarketServer/dto/ReviewRequestDto.java
@@ -0,0 +1,32 @@
+package Graduation.work.YongduriMarketServer.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewRequestDto {
+
+    @Data
+    public static class ReviewIdDto{
+        private long reviewId;
+    }
+    @Data
+    public static class ReviewDetailDto{
+        private long reviewee_Id;
+    }
+    @Data
+    public static class ReviewWriteDto{
+        private Long reviewId;
+        private Long board_Id;
+        private Long reviewee_Id;
+        private Long reviewer_Id;
+        private Integer assessment;
+        private Integer scope;
+        private Integer reviewStatus = 1;
+        private LocalDateTime createdAt;
+    }
+}
---
+++ src/main/java/Graduation/work/YongduriMarketServer/repository/ReviewRepository.java
@@ -0,0 +1,8 @@
+package Graduation.work.YongduriMarketServer.repository;
+
+import Graduation.work.YongduriMarketServer.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
